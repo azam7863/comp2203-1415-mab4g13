4aa20a9114d1a2ed5dd0a5f0f69b2bd7c07a83e5
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-05 19:47:42 +0000

    [2] Introduction message to the site and use of queries for the selection of random films from the database
    An introduction message was then written and implemented onto the
    homepage to welcome all users to the site; whilst, also giving them a
    flavour of what to expect within the sites's contents.
    
    A random film generator was then implemented through the use of queries;
    to ensure a new film is present on the homepage every time it is
    reloaded.

--- Index.php
+++ Index.php
@@ -1,11 +1,26 @@
-<!-- require database and film class -->
-<?php require("includes/db.php"); ?>
-<?php require("classes/film.class.php"); ?>
+!-- homepage-->
 
-<!--open body -->
-<body>
-<!--open container -->
-<div class ="container">
-<!--header -->
-<?php require ("includes/header.php");  ?>
- 
+<!-- introducing the site  -->
+	<div class="row" align="center">
+		<h1><b>Watch the Latest and Greatest on Red Carpet Films!</b></h1>
+		<h4><br>Explore new worlds through our exhilarating thrillers or re-live the past through our countless classics! </br>
+		<br>Red Carpet Films is the hub of the Film industry. Available to you at the click of a mouse.
+		Take the journey of a lifetime and walk into the world of Red Carpet Films.</h4>
+	</div>
+	
+		<!--random film-->
+		<div class="col-md-4">
+			<?php
+				/**
+				* extrapolates a random film from the database, every time the website is loaded
+				* @param filmname, filmid Object $random
+				* @prints img
+				*/
+				$random= rand (1,78);
+				$filmId = $db-> query("SELECT * FROM `comp2203-cw-1415`.film WHERE id =  $random")-> fetch_object()->id;
+				$filmName =$db->query ("SELECT * FROM `comp2203-cw-1415`.film WHERE id =  $random")-> fetch_object()->name;
+				/* partials for random film */
+				require ("partials/film-random.php");
+			?>			
+		</div>
+		
