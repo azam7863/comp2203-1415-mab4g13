6f6b7339c000ac166f536baba495f27073ae8417
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-05 15:21:31 +0000

    Implementation and structuring of the header section
    Initial use of the 'require' statement selects the content of 'head.php'
    and transfers it into this class.
    
    Creation of the banner then follows; with the assignment of its location
    'col-md-4' and the formation of the Title of my site within the banner
    as 'Red Carpet Films'.
    
    A concluding require statement is made in relation to the
    'navigation.php' class; to initiate its link within the class.

---
+++ header.php
@@ -0,0 +1,15 @@
+<!-- require head -->
+<?php require('includes/head.php'); ?>
+	
+	<!-- header --> 
+	<div id="header">
+		<div class="row">	
+		 <!-- banner -->
+		 <div class="col-md-4"><img src="img/banner.png" alt="Red Carpet Films" id="banner">
+		 </div> 
+		</div>
+
+			<!-- require navigation -->
+			<?php require('includes/navigation.php'); ?>
+			
+	</div>
