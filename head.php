59e5fc57b851ce56be16876ac92959a9f61c6e03
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-05 14:38:01 +0000

    Insertion of the Head section for the site
    This contains the Information within the header section; with variables
    of width and Initial scale assigned.
    
    A connection is also established with the CSS (Internal) and Bootstraps
    (External) stylesheets. Used to develop the structure and style of the
    header's contents.

---
+++ head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<!-- title -->
+		<title>Red Carpet Films | Film Rentals Company</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	   	<script src="../../assets/js/html5shiv.js"></script>
+		<script src="../../assets/js/respond.min.js"></script>
+		<![endif]-->
+		<!-- personal stylesheet -->
+		<link href='css/style.css' rel='stylesheet' media='screen'>
+		<!-- bootstrap stylesheet -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+  
