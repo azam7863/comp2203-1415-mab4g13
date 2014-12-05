bc19200aefc177bd8fe992db3c3e6175c30f4704
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-05 18:19:34 +0000

    [1] Construction of 'Red Carpet Films' Homepage for the site
    Firstly, Two require statements are implemented; they connect with the
    'db.php' and 'film.class.php' respectively.
    
    The body and container are then both opened for the homepage.
    
    Followed by a header; which instead includes the 'header.php' class
    which outputs a warning before letting the script continue.

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!-- require database and film class -->
+<?php require("includes/db.php"); ?>
+<?php require("classes/film.class.php"); ?>
+
+<!--open body -->
+<body>
+<!--open container -->
+<div class ="container">
+<!--header -->
+<?php require ("includes/header.php");  ?>
+ 
