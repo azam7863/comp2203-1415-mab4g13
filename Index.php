ba886c33a499772e2b49f159c0b2133b17d6f7f7
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-06 12:14:25 +0000

    [3] Creation of the interactive film carousel for my homepage
    Programmed the carousel to display the five most recent film releases
    all done according to the specification.
    
    Utilisation of  lists and ordered lists to initiate the creation
    process.

--- Index.php
+++ Index.php
@@ -1,26 +1,16 @@
-!-- homepage-->
+			<!-- carousel showing the 5 most recent film dvd releases -->
+			<div class="row">
+				<div class="col-md-7">
+					<div class="padding"><h4><b>Now on DVD!</b></h4></div>
+						<div id="carousel-example-generic" data-ride="carousel">
+						
+						<ol class="carousel-indicators">
+							<li data-target='#carousel-example-generic' data-slide-to='0' class='active'></li>
+							<li data-target='#carousel-example-generic' data-slide-to='1'></li>
+							<li data-target='#carousel-example-generic' data-slide-to='2'></li>
+							<li data-target='#carousel-example-generic' data-slide-to='3'></li>
+							<li data-target='#carousel-example-generic' data-slide-to='4'></li>
+						</ol>
+						<div class='carousel-inner'> 
 
-<!-- introducing the site  -->
-	<div class="row" align="center">
-		<h1><b>Watch the Latest and Greatest on Red Carpet Films!</b></h1>
-		<h4><br>Explore new worlds through our exhilarating thrillers or re-live the past through our countless classics! </br>
-		<br>Red Carpet Films is the hub of the Film industry. Available to you at the click of a mouse.
-		Take the journey of a lifetime and walk into the world of Red Carpet Films.</h4>
-	</div>
-	
-		<!--random film-->
-		<div class="col-md-4">
-			<?php
-				/**
-				* extrapolates a random film from the database, every time the website is loaded
-				* @param filmname, filmid Object $random
-				* @prints img
-				*/
-				$random= rand (1,78);
-				$filmId = $db-> query("SELECT * FROM `comp2203-cw-1415`.film WHERE id =  $random")-> fetch_object()->id;
-				$filmName =$db->query ("SELECT * FROM `comp2203-cw-1415`.film WHERE id =  $random")-> fetch_object()->name;
-				/* partials for random film */
-				require ("partials/film-random.php");
-			?>			
-		</div>
-		
+						<?php
