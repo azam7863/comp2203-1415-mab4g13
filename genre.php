5f93e52970c7626a1d862fe7a19a3d844d01364b
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-06 18:18:57 +0000

    [3] IF statement containing the Number of films displayed on each row; and partials for the selected film genre
    The If statement states the count should be divided by four (Number of
    films displayed on each row) equals 0.
    
    The insertion of a partial with an inclusive require statement for the
    selected film genre.

--- genre.php
+++ genre.php
@@ -1,13 +1,16 @@
-/*count up for each film*/
-					$count = 0;
-					$films = $db->query("SELECT * FROM `comp2203-cw-1415`.film WHERE genre_id = $id;");
-					echo("<div class='row'>");
-						
-					/**
-					* while statement that goes through each film in the database and shows their information on the genre page
-					* @param filmOb Object $films
-					* @print string, img
-					**/	
-					while ($filmOb = $films->fetch_object("filmtable"))
-						{	
-					
+/*if the $count divided by 4 (number of films displayed on each row) equals 0*/
+							if($count % 4 == 0) echo("</div>");
+								/* partials for selected film genre */
+								require ("partials/film-genre.php");
+							
+							/*increase the $count by 1*/
+							$count++;
+							if($count % 4 == 0) echo("<div class='row'>");	
+						}
+							echo("</div>");	
+	?>
+</div>
+<!-- end of genre page -->
+
+<!-- require footer -->
+<?php require('includes/footer.php'); ?>
