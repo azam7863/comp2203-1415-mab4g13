e8d9c895fc553016bb28e7e10e31ca4269a47599
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-06 13:24:24 +0000

    Creation of the Film class; used to select the film descriptions from within the database
    The film class is created to extrapolate all the relevant film
    description criteria from the database. Variables like Runtime,
    Director, Name and ID ETC are selected.

---
+++ film.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*extrapolates film descriptions from database to show in film pages*/
+class filmtable
+{
+	public $id;
+	public $genre_id;
+	public $name;
+	public $description;		
+	public $runtime;
+	public $director;
+	public $classification;
+	public $theatricalRelease;
+	public $dvdRelease;
+	public $rt_id;
+}
