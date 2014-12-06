41a2ccac66de3097f5146df9bec445ee9755dee9
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-06 13:31:32 +0000

    Creation of the Review class; selects the review information entered by the customers
    Review data  input by consumers is displayed from the database into this
    table.
    
    The  variables of ID, Film ID, Liked, Comment and reviewer are all
    extracted.

---
+++ review.class.php
@@ -0,0 +1,12 @@
+<?php
+	
+/*extrapolates review information entered by customers, from the database and shows in the review table*/
+class reviewtable
+{
+	public $id;
+	public $film_id;
+	public $liked;
+	public $comment;
+	public $reviewer;
+}
+	
