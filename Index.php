134675e9acc73bd576e92f73495785fbab0ee257
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-06 12:35:27 +0000

    [5] Final stage for the completion of the homepage; linking the poster for the carousel
    Chosen image from the allotted posters section was then integrated onto
    the carousel; to finalise its completion.
    
    And the end of the homepage was conclude with a final require statement;
    to the 'footer.php' class.

--- Index.php
+++ Index.php
@@ -1,22 +1,32 @@
-			/**
-							* extrapolates 5 most recent dvd releases (order by date of release) from film table in database 
-							* @param filmOb Object $fiveRelease
-							* @print img
-							*/	
-							$recent = new filmtable;
-							$fiveRelease = $recent = $db->query("SELECT * FROM `comp2203-cw-1415`.film ORDER BY dvdRelease ASC LIMIT 5");
-							$count = 0;
-							
-								while ($filmOb = $fiveRelease->fetch_object("filmtable"))
-								{
-									if($count == 0) 
-									{	
-										$active = "active";
-									}
-										else
-										{
-											$active = "";
-										}
-									
-									echo("<div class='item $active carousel_Img_Size'>");	
-									echo("<a href='film.php?id=".$filmOb->id."'>");
+			/*massive poster for the carousel*/
+									echo("<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/".$filmOb->id."_massive.jpg'/>");
+									echo("</a>");
+									echo("</div>");
+									$count++;
+								}
+			
+							?>
+						</div>
+				
+			
+						<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+							<div id="previous">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							</div>
+						</a>
+						
+						<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+							<div id="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							</div>
+						</a>
+						</div>
+					<br>
+				</div>
+				<!-- end of carousel -->
+			</div>				
+</div>
+<!--end of homepage-->
+
+<!-- require footer -->
+<?php require ("includes/footer.php");?></div>
