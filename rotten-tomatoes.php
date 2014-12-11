411df1de16d087aa9854a7c1c20e0e8746163d77
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-11 12:37:52 +0000

    An extended rotten toamtoes class was created to display the audience and critic scores
    Partial were utilised to help gather the official rotten tomatoes API
    ratings specifically for each movie.

---
+++ rotten-tomatoes.php
@@ -0,0 +1,8 @@
+<!-- partial for gathering audience & critic ratings from the rotten tomatoes API -->
+<p>
+	<b>Audience Rating: </b><?$rotten_tomatoes->ratings->audience_score?><b>%</b><br><p>
+    <b>Critic Rating: </b><?$rotten_tomatoes->ratings->critics_score?><b>%</b>
+</p>
+
+	</div>
+</div>
