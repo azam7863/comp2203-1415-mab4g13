56dd13f6cc625d53c0a8af9840ceaf154704c305
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-11 11:35:15 +0000

    The film list section was then implemented
    Through the use of a partial to display the relevant and specified film
    images. Processing this action was done through inclusion of the medium
    image URL link within the image source.

---
+++ film-list.php
@@ -0,0 +1,10 @@
+<!-- Partial for displaying all film images -->
+<div class='text-center col-md-3' id='images'> 
+	<a href='film.php?id=<?=$film->id?>'> 
+		<!-- Medium image -->
+		<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/<?=$film->id?>_medium.jpg' class='img'/> 
+	</a> 
+		<p></p> 
+		     <h5><p><?=$film->name?></p></h5><br> 
+</div> 
+				
