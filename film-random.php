af104f049482af3de40d0c08cb4a3c358d33700a
azam7863 <mab4g13@ecs.soton.ac.uk>
2014-12-11 11:38:09 +0000

    Completion of the random film generator for the homepage was then required
    An initial starting point was formed by creating a partial for the
    random film section. An through the introduction of a header above the
    random film poster reading 'Rent out Now!'.

---
+++ film-random.php
@@ -0,0 +1,6 @@
+<!-- Partial for the random film  -->
+<h3><b>Rent out Now!</b></h3>
+	<a href = 'film.php?id=<?=$filmId?>'>
+	<!--medium image-->
+	<img src='http://comp2203.ecs.soton.ac.uk/coursework/1415/assets/posters/<?=$random?>_medium.jpg' id='random'/>
+	</a><br>
